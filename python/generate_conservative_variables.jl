372f911e11ef5c6c40311b23cb0c18c1d011fcaf
ScSteffen <steffen.schotthoefer@outlook.de>
2021-12-08 15:25:00 +0100

    added helper jl skript

--- python/generate_conservative_variables.jl
+++ python/generate_conservative_variables.jl
@@ -1,10 +1,53 @@
 """
 Small helper script to generate conservative variables from sampled kinetic densities in julia
 """
-using
-KitBase, Plots, JLD2, Distributions, LinearAlgebra, Flux
-using
-Flux:
+using KitBase, Plots, JLD2, Distributions, LinearAlgebra, Flux
+using Flux: @epochs
 
+function regime_data(w, sw, f, u, K, Kn, μ=ref_vhs_vis(Kn, 1.0, 0.5), ω=0.81)
+    gam = heat_capacity_ratio(K, 1)
+    prim = conserve_prim(w, gam)
+    Mu, Mxi, _, _1 = gauss_moments(prim, K)
+    a = pdf_slope(prim, sw, K)
+    swt = -prim[1] .* moments_conserve_slope(a, Mu, Mxi, 1)
+    A = pdf_slope(prim, swt, K)
+    tau = vhs_collision_time(prim, μ, ω)
+    fr = chapman_enskog(u, prim, a, A, tau)
+    L = norm((f .- fr) ./ prim[1])
 
-@epochs
+    x = [w; sw; tau]
+    y = ifelse(L <= 0.005, 0.0, 1.0)
+    return x, y
+end
+
+function regime_number(Y, rg=0)
+   idx = 0
+    for i in axes(Y, 2)
+       if Y[1, i] == rg
+            idx += 1
+        end
+    end
+    println("NS regime: $(idx) of $(size(Y, 2))")
+    return nothing
+end
+
+function accuracy(nn, X, Z)
+    Z1 = nn(X)
+
+    ZA1 = [round(Z1[1, i]) for i in axes(Z1, 2)]
+    ZA = [round(Z[1, i]) for i in axes(Z, 2)]
+
+    accuracy = 0.0
+    for i in eachindex(ZA)
+        if ZA[i] == ZA1[i]
+            accuracy += 1.0
+        end
+    end
+    accuracy /= length(ZA)
+
+    return accuracy
+end
+
+# Load Data
+
+file = open("../../../data/1d/a2_ev10.csv")
