07ffdd0bbd2e5328785fb877761044fcfb31389a
Steffen <steffen.schotthoefer@kit.edu>
2022-01-07 10:14:46 +0100

    data distribution illustration

--- illustration/view_entropydynamics.py
+++ illustration/view_entropydynamics.py
@@ -7,56 +7,59 @@ Date 21.12.2021
 
 import numpy as np
 import pandas as pd
-from utils import load_density_function2D, load_density_function, plot_density_fusion_1d, plot_densities, plot_1d
-from entropytools import EntropyTools, qGaussLegendre1D
+from utils import load_density_function, scatter_plot_2d_N2, scatter_plot_3d, scatter_plot_2d
 
 
 def paper_illustrations():
+    # ---- illustrate moment dynamics
     x_dat = np.load('data/sod1D/X.npy')
     y_dat = np.load('data/sod1D/Y.npy')
-    z_dat = np.load('data/sod1D/Z.npy')  #
+    z_dat = np.load('data/sod1D/Z.npy')
+    iter_dat = np.load('data/sod1D/I.npy')
 
+    # 1) moment - Sod test case
     moment_u = x_dat[:3, :]
     # normalize everything
     u_normal = moment_u / moment_u[0, :]
-    
-    """ Skript that performs all illustrations for the paper done by Steffen"""
-    [v_x, weights, f_kinetic] = load_density_function("data/pdfs.csv")
-    f_ns = pd.read_csv("data/fns.csv").to_numpy()
-    grads = pd.read_csv("data/gradient_w.csv").to_numpy()
-    params = pd.read_csv("data/paras.csv").to_numpy()
-    conservative_variables = pd.read_csv("data/w.csv").to_numpy()
+    scatter_plot_2d_N2(x_in=u_normal[1:, :].reshape((u_normal.shape[1], 2)), z_in=iter_dat[0], show_fig=False,
+                       log=False,
+                       folder_name='illustrations', color_map=1, name='moment_dynamics', label_x='velocity',
+                       label_y='temperature', title='macroscopic variables over time')
 
-    # ----- Ilustrate Upwing Merging of two pdf -----
-    v_x = np.reshape(v_x, newshape=(v_x.shape[1]))
+    # 2) gradient - sod test case
+    grad_u = x_dat[3:6, :]
+    scatter_plot_3d(xyz_in=grad_u.reshape((u_normal.shape[1], 3)), color_in=iter_dat[0], show_fig=False, log=False,
+                    folder_name='illustrations', color_map=1, name='grad_moment_dynamics',
+                    title='gradient macroscopic variables over time', lim_x=(-1, 10),
+                    lim_y=(0, 10), lim_z=(0, 10))
+    scatter_plot_2d(x_in=grad_u[1:, :].reshape((u_normal.shape[1], 2)), z_in=iter_dat[0], show_fig=False, log=False,
+                    folder_name='illustrations', color_map=1, name='grad_moment_dynamics2D', label_x=r"$\nabla_x U$",
+                    label_y=r"$\nabla_x T$", title='gradient macroscopic variables over time')
 
-    # plot_density_fusion_1d(v_x=v_x, f_l=f_kinetic[0], f_r=f_kinetic[1], f_fuse=f_kinetic[2], f_ns=f_ns[:, 0],
-    #                       show_fig=False, save_name='maxwellians_fusion')
-    # plot_density_fusion_1d(v_x=v_x, f_l=f_kinetic[3], f_r=f_kinetic[4], f_fuse=f_kinetic[5], f_ns=f_ns[:, 1],
-    #                       show_fig=False, save_name='bimodal_fusion')
+    # II ----- illustrate generated data
+    x_dat = np.load('data/generator1D/X.npy')
+    y_dat = np.load('data/generator1D/Y.npy')
+    # 1) moment - generated
+    moment_u = x_dat[:3, :]
+    # normalize everything
+    u_normal = moment_u / moment_u[0, :]
+    scatter_plot_2d_N2(x_in=u_normal[1:, :].reshape((u_normal.shape[1], 2)), z_in=np.zeros(shape=(u_normal.shape[1])),
+                       show_fig=False, log=False,
+                       folder_name='illustrations', color_map=1, name='moment_dynamics_genData', label_x='velocity',
+                       label_y='temperature', title='macroscopic variables (generated)')
 
-    plot_1d(xs=[v_x], ys=[f_kinetic[0, :].reshape(len(f_kinetic[0]), 1), f_kinetic[1, :].reshape(len(f_kinetic[0]), 1),
-                          f_kinetic[2, :].reshape(len(f_kinetic[0]), 1), f_ns[:, 0].reshape(len(f_kinetic[0]), 1)],
-            labels=['left cell', 'right cell', 'interface', 'BGK reconstruction'], name='maxwell_fusion', log=False,
-            folder_name="illustrations", linetypes=['-', '--', 'o', '-.'], show_fig=False, xlim=(-5, 5), ylim=(0, 0.75),
-            xlabel="velocity", ylabel="density", title=" ")
-    plot_1d(xs=[v_x], ys=[f_kinetic[3, :].reshape(len(f_kinetic[0]), 1), f_kinetic[4, :].reshape(len(f_kinetic[0]), 1),
-                          f_kinetic[5, :].reshape(len(f_kinetic[0]), 1), f_ns[:, 1].reshape(len(f_kinetic[0]), 1)],
-            labels=['left cell', 'right cell', 'interface', 'BGK reconstruction'], name='bimodal_fusion', log=False,
-            folder_name="illustrations", linetypes=['-', '--', 'o', '-.'], show_fig=False, xlim=(-5, 5), ylim=(0, 0.75),
-            xlabel="velocity", ylabel="density", title=" ")
-
-    plot_1d(xs=[v_x], ys=[f_kinetic[0, :].reshape(len(f_kinetic[0]), 1), f_kinetic[3, :].reshape(len(f_kinetic[0]), 1),
-                          f_kinetic[8, :].reshape(len(f_kinetic[0]), 1)],
-            labels=['Maxwellian', 'Bimodal', 'Highly anisotropic'],
-            name='Entropy_Sampling', log=False,
-            folder_name="illustrations", linetypes=None,
-            show_fig=False, xlim=(-5, 5), ylim=(0, 0.75), xlabel="velocity", ylabel="density",
-            title=" ")
-
-    # plot_densities(v_x=v_x, f_maxwell=f_kinetic[0], f_entropy=f_kinetic[4], f_fourier=f_kinetic[6],
-    #               f_random=f_kinetic[7], f_unlikely=f_kinetic[8], show_fig=False, save_name='example_densities')
-
+    # 2) gradient - generated
+    grad_u = x_dat[3:6, :]
+    scatter_plot_3d(xyz_in=grad_u.reshape((u_normal.shape[1], 3)),
+                    color_in=np.zeros(shape=(u_normal.shape[1])), show_fig=False, log=False,
+                    folder_name='illustrations', color_map=1, name='grad_moment_dynamics_genData',
+                    title='gradient macroscopic variables (generated)', lim_x=(-1, 10),
+                    lim_y=(0, 10), lim_z=(0, 10))
+    scatter_plot_2d(x_in=grad_u[1:, :].reshape((u_normal.shape[1], 2)),
+                    z_in=np.zeros(shape=(u_normal.shape[1])), show_fig=False, log=False,
+                    folder_name='illustrations', color_map=1, name='grad_moment_dynamics2D_genData',
+                    label_x=r"$\nabla_x U$", label_y=r"$\nabla_x T$",
+                    title='gradient macroscopic variables (generated)')
     return 0
 
 
